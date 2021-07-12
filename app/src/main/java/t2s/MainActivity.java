2be7eed7bb4051fc17f4dfd0bbfd5773ccbf0753
Alan-Biju <alanbiju238@gmail.com>
2021-07-12 14:03:47 +0530

    'tesing'

---
+++ app/src/main/java/t2s/MainActivity.java
@@ -0,0 +1,54 @@
+package t2s;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.t2s.R;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public EditText editText;
+    public TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.convertButton);
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR)
+                    textToSpeech.setLanguage(Locale.US);
+            }
+        });
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String text = editText.getText().toString();
+        if (text.equals(""))
+            makeToast("EMPTY");
+        else
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private void makeToast(String toastMessage) {
+        Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+    }
+}
+
+
